649c1a1f2840bb33702ca54f3439a9c3712ab87f
avrshaf <aurorashafasalsabilaa07@gmail.com>
2026-06-14 21:37:37 +0700

    Update medicines.php

--- DigiPharm/medicines.php
+++ DigiPharm/medicines.php
@@ -2,7 +2,6 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); 
 
-// Database array objek obat - LENGKAP 12 ITEM sesuai file catalog.html asli milikmu
 $medicines = [
     ["id" => "paracetamol", "name" => "Paracetamol", "dose" => "500mg", "price" => "10.000"],
     ["id" => "ibuprofen", "name" => "Ibuprofen", "dose" => "400mg", "price" => "15.000"],
